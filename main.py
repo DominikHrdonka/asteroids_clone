16e2e151c1356fa9220dedaab85fcf9fa383914b
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-08-30 14:08:49 +0200

    create method to rotate player object left and right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 
 def main():
@@ -10,8 +10,9 @@ def main():
 
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-        player = Player(x= SCREEN_WIDTH /2, y= SCREEN_HEIGHT/2, radius=PLAYER_RADIUS, position = pygame.math.Vector2(SCREEN_WIDTH/2, SCREEN_HEIGHT/2), rotation=0)
+        player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT/2)
 
+# Main game loop:
         while True:
               for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -20,6 +21,7 @@ def main():
               screen.fill("black")
               player.draw(screen)
               pygame.display.flip()
+              player.update(dt)
               
               dt = clock.tick(60)/1000
 
--- player.py
+++ player.py
@@ -1,11 +1,11 @@
 import pygame
 from circleshape import *
+from constants import *
 
 class Player(CircleShape):
-    def __init__(self, x, y, radius, position=None , rotation=0):
-        super().__init__(x, y, radius)
-        self.position = pygame.math.Vector2(position) if position else pygame.math.Vector2(x, y)
-        self.rotation = rotation
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
 
 
         # in the player class
@@ -19,4 +19,17 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), width = 2)
-        return super().draw(screen)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
+
+    
