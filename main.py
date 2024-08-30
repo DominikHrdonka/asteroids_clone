911e0b17aba42ae1d9128935b0581350aa04b0a9
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-08-30 13:33:47 +0200

    create Player child class and initialize player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -9,13 +10,17 @@ def main():
 
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+        player = Player(x= SCREEN_WIDTH /2, y= SCREEN_HEIGHT/2, radius=PLAYER_RADIUS, position = pygame.math.Vector2(SCREEN_WIDTH/2, SCREEN_HEIGHT/2), rotation=0)
+
         while True:
               for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         return
                 
               screen.fill("black")
+              player.draw(screen)
               pygame.display.flip()
+              
               dt = clock.tick(60)/1000
 
 
